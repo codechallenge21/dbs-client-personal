bbd2b5d3468b94d1eb76a137454fe59c4985cbd2
Suleman Ahmad <sulemanmuqeed@gmail.com>
2024-12-16 20:52:24 +0500

    add dialog box

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,12 +1,29 @@
+"use client";
+import { useState } from "react";
 import { Box } from "@mui/material";
 import Header from "./components/Header";
 import MainContent from "./components/MainContent";
+import SwitchDialog from "./components/SwitchDialog";
 
 export default function Home() {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  // const handleOpen = () => setIsOpen(true);
+  const handleClose = () => setIsOpen(false);
+
+  const handleConfirm = () => {
+    console.log("Switched to 意外事件顧問");
+    setIsOpen(false);
+  };
   return (
     <Box>
       <Header />
       <MainContent />
+      <SwitchDialog
+        open={isOpen}
+        onClose={handleClose}
+        onConfirm={handleConfirm}
+      />
     </Box>
   );
 }
