76dafb18eeef40b345b66432278c60d69769acc8
Abdul Mannan <abdulmannan3111@gmail.com>
2025-01-23 02:48:40 +0500

    move context to root

---
+++ src/components/channel-context-provider/ChannelContextProvider.tsx
@@ -0,0 +1,58 @@
+'use client'; // Mark this file as a client component
+
+import React, { useState, useMemo } from 'react';
+import ChannelContentContext from '@/app/chat/components/ChannelContentContext';
+import {
+  OrganizationChannel,
+  OrganizationChannelMessage,
+} from '@/interfaces/entities';
+import { AdvisorType } from '@/app/chat/components/types';
+
+export default function ChannelContextProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [isLoadingChannel, setIsLoadingChannel] = useState(false);
+  const [selectedChannel, setSelectedChannel] = useState<OrganizationChannel>();
+  const [selectedChannelId, setSelectedChannelId] = useState<string>();
+  const [isInteractingInChat, setIsInteractingInChat] =
+    useState<boolean>(false);
+  const [chatResponses, setChatResponses] = useState<
+    OrganizationChannelMessage[]
+  >([]);
+  const [advisorType, setAdvisorType] = useState<AdvisorType>(
+    AdvisorType.DEFAULT
+  );
+
+  const contextValue = useMemo(
+    () => ({
+      isLoadingChannel,
+      setIsLoadingChannel,
+      selectedChannel,
+      setSelectedChannel,
+      selectedChannelId,
+      setSelectedChannelId,
+      isInteractingInChat,
+      setIsInteractingInChat,
+      chatResponses,
+      setChatResponses,
+      advisorType,
+      setAdvisorType,
+    }),
+    [
+      isLoadingChannel,
+      selectedChannel,
+      selectedChannelId,
+      isInteractingInChat,
+      chatResponses,
+      advisorType,
+    ]
+  );
+
+  return (
+    <ChannelContentContext.Provider value={contextValue}>
+      {children}
+    </ChannelContentContext.Provider>
+  );
+}
