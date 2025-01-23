2cc68898ce4f5b3d87c493dd815c9dbe0a66e0a4
Suleman Ahmad <sulemanmuqeed@gmail.com>
2025-01-23 14:19:52 +0500

    add wishing well page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,46 +1,44 @@
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import ThemeProvider from "@/theme";
-import { Box } from "@mui/material";
-import { Header } from "@/components/header";
+import { Geist, Geist_Mono } from 'next/font/google';
+import './globals.css';
+import ThemeProvider from '@/theme';
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
 });
 
 export const metadata = {
-  title: "好理家在｜好好理財，家就會在",
-  description: "好理家在官方網站",
-  keywords: "react,material,kit,application,dashboard,admin,template",
-  themeColor: "#000000",
-  manifest: "/manifest.json",
+  title: '好理家在｜好好理財，家就會在',
+  description: '好理家在官方網站',
+  keywords: 'react,material,kit,application,dashboard,admin,template',
+  themeColor: '#000000',
+  manifest: '/manifest.json',
   icons: [
     {
-      rel: "icon",
-      url: "/favicon/favicon.svg",
+      rel: 'icon',
+      url: '/favicon/favicon.svg',
     },
     {
-      rel: "icon",
-      type: "image/svg",
-      sizes: "16x16",
-      url: "/favicon/favicon.svg",
+      rel: 'icon',
+      type: 'image/svg',
+      sizes: '16x16',
+      url: '/favicon/favicon.svg',
     },
     {
-      rel: "icon",
-      type: "image/svg",
-      sizes: "32x32",
-      url: "/favicon/favicon.svg",
+      rel: 'icon',
+      type: 'image/svg',
+      sizes: '32x32',
+      url: '/favicon/favicon.svg',
     },
     {
-      rel: "apple-touch-icon",
-      sizes: "180x180",
-      url: "/favicon/favicon.svg",
+      rel: 'apple-touch-icon',
+      sizes: '180x180',
+      url: '/favicon/favicon.svg',
     },
   ],
 };
@@ -53,9 +51,6 @@ export default function RootLayout({ children }: Props) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <Box>
-          <Header />
-        </Box>
         <ThemeProvider>{children}</ThemeProvider>
       </body>
     </html>
