73792afcd09b4d66518821f082088e1918a8f28c
Suleman Ahmad <sulemanmuqeed@gmail.com>
2025-01-29 21:40:06 +0500

    add wishing well page

--- src/app/wishing/page.tsx
+++ src/app/wishing/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 import React, { useRef, useState } from 'react';
-import { Box, Grid, Typography, IconButton } from '@mui/material';
-import { ArrowForwardIosRounded, WorkRounded } from '@mui/icons-material';
+import { Box, Typography, IconButton, Grid2 } from '@mui/material';
+import { ArrowForwardIosRounded } from '@mui/icons-material';
 import Image from 'next/image';
 import boxImage from '../../../public/assets/images/box.png';
 import ToolboxDrawer from '@/components/toolbox-drawer-new/ToolboxDrawer';
@@ -103,7 +103,110 @@ export default function Home() {
             backgroundColor: 'white',
           }}
         >
-          <Box sx={{ position: 'relative', marginBottom: '40px' }}>
+          <Box>
+            <Box sx={{ position: 'relative', marginBottom: '40px' }}>
+              <Typography
+                gutterBottom
+                sx={{
+                  fontWeight: 400,
+                  fontSize: '24px',
+                  fontStyle: 'normal',
+                  lineHeight: 'normal',
+                  marginBottom: '16px',
+                  fontFamily: 'DFPHeiBold-B5',
+                  color: 'var(--Primary-Black, #212B36)',
+                }}
+              >
+                你可能感興趣
+              </Typography>
+              <Box
+                sx={{
+                  display: 'flex',
+                  position: 'relative',
+                }}
+              >
+                <Box
+                  ref={focusRef}
+                  sx={{
+                    gap: '16px',
+                    cursor: 'grab',
+                    display: 'flex',
+                    overflow: 'hidden',
+                    marginBottom: '20px',
+                    width: '100%',
+                  }}
+                >
+                  {focusItems.map((_, index) => (
+                    <Box
+                      key={index}
+                      sx={{
+                        display: 'flex',
+                        minWidth: '260px',
+                        minHeight: '220px',
+                        alignItems: 'center',
+                        flexDirection: 'column',
+                        justifyContent: 'center',
+                      }}
+                    >
+                      <Image
+                        src={boxImage}
+                        alt="Boxed Image"
+                        style={{
+                          width: '100%',
+                          height: '130px',
+                          borderTopLeftRadius: '8px',
+                          borderTopRightRadius: '8px',
+                        }}
+                      />
+                      <Box
+                        sx={{
+                          width: '100%',
+                          borderBottomLeftRadius: '8px',
+                          borderBottomRightRadius: '8px',
+                          backgroundColor: 'var(--Primary-, #EBE3DD)',
+                        }}
+                      >
+                        <Typography
+                          sx={{
+                            marginLeft: '20px',
+                            marginTop: '10px',
+                            fontFamily: 'DFPHeiBold-B5',
+                            color: 'var(--Primary-Black, #212B36)',
+                          }}
+                        >
+                          標題
+                        </Typography>
+                        <Typography
+                          variant="body2"
+                          color="textSecondary"
+                          sx={{ marginLeft: '20px', marginBottom: '20px' }}
+                        >
+                          説明文字
+                        </Typography>
+                      </Box>
+                    </Box>
+                  ))}
+                </Box>
+                <IconButton
+                  onClick={handleScrollRight}
+                  sx={{
+                    top: '80px',
+                    right: '2px',
+                    '&:hover': {
+                      backgroundColor: 'rgba(204, 0, 0, 0.40)',
+                    },
+                    zIndex: 10,
+                    position: 'absolute',
+                    backgroundColor: 'rgba(204, 0, 0, 0.60)',
+                  }}
+                >
+                  <ArrowForwardIosRounded
+                    sx={{ width: '18px', height: '18px', color: 'white' }}
+                  />
+                </IconButton>
+              </Box>
+            </Box>
+
             <Typography
               gutterBottom
               sx={{
@@ -116,133 +219,67 @@ export default function Home() {
                 color: 'var(--Primary-Black, #212B36)',
               }}
             >
-              你可能感興趣
+              所有活動
             </Typography>
-            <Box
+            <Grid2
+              container
+              spacing={2}
               sx={{
-                display: 'flex',
-                position: 'relative',
+                marginBottom: '20px',
               }}
             >
-              <Box
-                ref={focusRef}
-                sx={{
-                  gap: '16px',
-                  cursor: 'grab',
-                  display: 'flex',
-                  overflow: 'hidden',
-                  marginBottom: '20px',
-                  width: '100%',
-                }}
-              >
-                {focusItems.map((_, index) => (
-                  <Box
-                    key={index}
-                    sx={{
-                      display: 'flex',
-                      minWidth: '260px',
-                      minHeight: '220px',
-                      alignItems: 'center',
-                      flexDirection: 'column',
-                      justifyContent: 'center',
-                    }}
-                  >
-                    <Image
-                      src={boxImage}
-                      alt="Boxed Image"
-                      style={{
-                        width: '100%',
-                        height: '130px',
-                        borderTopLeftRadius: '8px',
-                        borderTopRightRadius: '8px',
-                      }}
-                    />
-                    <Box
-                      sx={{
-                        width: '100%',
-                        borderBottomLeftRadius: '8px',
-                        borderBottomRightRadius: '8px',
-                        backgroundColor: 'var(--Primary-, #EBE3DD)',
-                      }}
-                    >
-                      <Typography
-                        sx={{
-                          marginLeft: '20px',
-                          marginTop: '10px',
-                          fontFamily: 'DFPHeiBold-B5',
-                          color: 'var(--Primary-Black, #212B36)',
-                        }}
-                      >
-                        標題
-                      </Typography>
-                      <Typography
-                        variant="body2"
-                        color="textSecondary"
-                        sx={{ marginLeft: '20px', marginBottom: '20px' }}
-                      >
-                        説明文字
-                      </Typography>
-                    </Box>
-                  </Box>
-                ))}
-              </Box>
-              <IconButton
-                onClick={handleScrollRight}
-                sx={{
-                  top: '80px',
-                  right: '2px',
-                  '&:hover': {
-                    backgroundColor: 'rgba(204, 0, 0, 0.40)',
-                  },
-                  zIndex: 10,
-                  position: 'absolute',
-                  backgroundColor: 'rgba(204, 0, 0, 0.60)',
-                }}
-              >
-                <ArrowForwardIosRounded
-                  sx={{ width: '18px', height: '18px', color: 'white' }}
-                />
-              </IconButton>
-            </Box>
-          </Box>
-
-          <Typography
-            gutterBottom
-            sx={{
-              fontWeight: 400,
-              fontSize: '24px',
-              fontStyle: 'normal',
-              lineHeight: 'normal',
-              marginBottom: '16px',
-              fontFamily: 'DFPHeiBold-B5',
-              color: 'var(--Primary-Black, #212B36)',
-            }}
-          >
-            工具下載
-          </Typography>
-          <Grid
-            container
-            spacing={2}
-            sx={{
-              marginBottom: '20px',
-            }}
-          >
-            {toolItems.map((_, index) => (
-              <Grid key={index} component="div" xs={6} sm={4} md={3} lg={2}>
-                <Box sx={{ padding: '16px', textAlign: 'center' }}>
-                  <WorkRounded
-                    sx={{
-                      width: '71px',
-                      height: '72px',
-                      color: 'black',
-                      marginBottom: '20px',
+              {toolItems.map((_, index) => (
+                <Box
+                  key={index}
+                  sx={{
+                    display: 'flex',
+                    minWidth: '260px',
+                    minHeight: '220px',
+                    alignItems: 'center',
+                    flexDirection: 'column',
+                    justifyContent: 'center',
+                  }}
+                >
+                  <Image
+                    src={boxImage}
+                    alt="Boxed Image"
+                    style={{
+                      width: '100%',
+                      height: '130px',
+                      borderTopLeftRadius: '8px',
+                      borderTopRightRadius: '8px',
                     }}
                   />
-                  <Typography variant="body2">工具名称</Typography>
+                  <Box
+                    sx={{
+                      width: '100%',
+                      borderBottomLeftRadius: '8px',
+                      borderBottomRightRadius: '8px',
+                      backgroundColor: 'var(--Primary-, #EBE3DD)',
+                    }}
+                  >
+                    <Typography
+                      sx={{
+                        marginLeft: '20px',
+                        marginTop: '10px',
+                        fontFamily: 'DFPHeiBold-B5',
+                        color: 'var(--Primary-Black, #212B36)',
+                      }}
+                    >
+                      標題
+                    </Typography>
+                    <Typography
+                      variant="body2"
+                      color="textSecondary"
+                      sx={{ marginLeft: '20px', marginBottom: '20px' }}
+                    >
+                      説明文字
+                    </Typography>
+                  </Box>
                 </Box>
-              </Grid>
-            ))}
-          </Grid>
+              ))}
+            </Grid2>
+          </Box>
         </Box>
       </ToolboxDrawer>
     </Box>
