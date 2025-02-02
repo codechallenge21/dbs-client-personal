a603969a03ce13b9d7fadb5c9697ce2fdede189b
Abdul Mannan <abdulmannan3111@gmail.com>
2025-02-02 14:53:57 +0500

    refactor: remove husky pre-commit hook and update TypeScript configuration; enhance hook types for chat channels

--- src/utils/hooks/useChatChannels.ts
+++ src/utils/hooks/useChatChannels.ts
@@ -1,14 +1,18 @@
-import makeGetHook from "@eGroupAI/hooks/apis/makeGetHook";
-import { fetcher } from "@/utils/hooks/apis/fetchers";
-import { OrganizationChannel } from "@/interfaces/entities";
+import makeGetHook from '@eGroupAI/hooks/apis/makeGetHook';
+import { fetcher } from '@/utils/hooks/apis/fetchers';
+import { OrganizationChannel } from '@/interfaces/entities';
 
 export type PathParams = {
   organizationId: string;
 };
 
-export const useChatChannels = makeGetHook<OrganizationChannel[], PathParams>(
-  "/organizations/{{organizationId}}/channels",
-  fetcher,
-  undefined,
-  { organizationChannelType: "CHAT" }
-);
+type UseChatChannelsType = ReturnType<
+  typeof makeGetHook<OrganizationChannel[], PathParams>
+>;
+
+export const useChatChannels: UseChatChannelsType = makeGetHook<
+  OrganizationChannel[],
+  PathParams
+>('/organizations/{{organizationId}}/channels', fetcher, undefined, {
+  organizationChannelType: 'CHAT',
+});
